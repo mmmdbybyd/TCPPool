60d6b353c5b31330f9b1dc689d996606a36dbd98
mmmdbybyd <915445800@qq.com>
2022-08-11 14:22:13 +0800

    Update pool_test.go

--- pool_test.go
+++ pool_test.go
@@ -18,7 +18,7 @@ func TestPool_Base(t *testing.T) {
 	}
 	pool.Start() //启动连接池
 	httpRequest := []byte("GET /d?dn=github.com HTTP/1.1\r\nHost: 119.29.29.29\r\nConnection: Close\r\n\r\n")
-	conn, err := pool.Get() //从连接池中取一个连接，，如果连接池中没有空闲连接则调用Dial()创建新的连接
+	conn, err := pool.Get() //从连接池中取一个连接，如果连接池中没有空闲连接则调用Dial()创建新的连接
 	if err != nil {
 		t.Error("pool.Get():", err)
 		return
@@ -58,14 +58,15 @@ func TestPool_Restart(t *testing.T) {
 				return
 			}
 			t.Log("TCP Dest Address:", conn.RemoteAddr().String())
-			defer conn.Close()
 			_, err = conn.Write(httpRequest)
 			if err != nil {
+				conn.Close()
 				t.Error("conn.Write():", err)
 				return
 			}
 			var rsp [2048]byte
 			_, err = conn.Read(rsp[:])
+			conn.Close()
 			if err != nil {
 				t.Error("conn.Read():", err)
 				return
